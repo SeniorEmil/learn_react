e481d216e856561733394a6bd57cc6aef2c4d0ce
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2022-04-24 01:09:34 +0300

    add infinity pagination - not decomposed

--- app/src/pages/Posts.jsx
+++ app/src/pages/Posts.jsx
@@ -22,6 +22,8 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const lastElement = useRef()
+  const observer = useRef()
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page)
@@ -29,6 +31,21 @@ function Posts() {
     const totalCount = (response.headers['x-total-count'])
     setTotalPages(getPageCount(totalCount, limit))
   })
+
+  useEffect(() => {
+    if(isPostsLoading) return
+    if(observer.current) observer.current.disconnect()
+    let callback = function (entries, observer) {
+      if (entries[0].isIntersecting && page < totalPages) {
+        console.log(page)
+        setPage(page + 1)
+      }
+
+    };
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current)
+  }, [isPostsLoading])
+
 
   useEffect(() => {
     fetchPosts()
@@ -66,6 +83,7 @@ function Posts() {
         <h1>Произошла ошибка {postError}</h1>
       }
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Reacts' />
+      <div ref={lastElement} style={{ height: 20, background: 'green' }} />
       {isPostsLoading &&
         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><MyLoader /></div>
       }
